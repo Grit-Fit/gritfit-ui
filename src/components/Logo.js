15f6a5ed126f8c509f9d6ed32669680da5e61ba2
Mishti-M <gritfit.dev@gmail.com>
2024-09-30 23:16:32 +0530

    fixing all stylings

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,26 +1,26 @@
 // src/components/LogoPage.js
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import logo from '../assets/Logo.png';
-import './Logo.css';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/Logo.png";
+import "./Logo.css";
 
 const LogoPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            navigate('/welcome');
-        }, 5000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/welcome");
+    }, 10000);
 
-        return () => clearTimeout(timer);
-    }, [navigate]);
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
-    return (
-        <div className="logo-container">
-            <img src={logo} alt="Logo" className="logo" />
-            <div className="tagline">A classy tagline ;)</div>
-        </div>
-    );
+  return (
+    <div className="logo-container">
+      <img src={logo} alt="Logo" className="logo" />
+      <div className="tagline">A classy tagline ;)</div>
+    </div>
+  );
 };
 
 export default LogoPage;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,43 +1,45 @@
 // src/components/WelcomePage.js
-import React, { useState } from 'react';
-import { useAuth } from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
-import logo from '../assets/Logo.png';
-import './Welcome.css'; 
-
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/Logo.png";
+import "./Welcome.css";
 
 const WelcomePage = () => {
-    const { logout } = useAuth();
-    const [name, setName] = useState('');
-    const navigate = useNavigate();
-    const handleSubmit = (e) =>{
-        setName(e.target.value);
-    };
-        const handleLogout = () => {
-        logout();
-        navigate('/');
-    };
-    return (
-        <div className="container">
-            <img src={logo} alt="Logo" className="logo" />
-            <h1 className="welcome">Welcome!</h1>
-            <p className="text">We are as excited as you are!</p>
-            <p className="text">Lets start by knowing a bit about you</p>
-            <h3 className="nameprompt" >What is your name?</h3>
-            <form onSubmit={handleSubmit}>
-                 <input
-                    type="text"
-                    value={name}
-                    className="inputbx"
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder="Enter your name"
-                    required
-                />
-                <button type="submit" className="btn" >Submit</button>
-                <button onClick={handleLogout} className="btn">Logout</button>
-            </form>
-        </div>
-    );
+  const { logout } = useAuth();
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    setName(e.target.value);
+  };
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+  return (
+    <div className="container">
+      <img src={logo} alt="Logo" className="logo" />
+      <h1 className="welcome">Welcome!</h1>
+      <p className="text">We are as excited as you are!</p>
+      <p className="text">Lets start by knowing a bit about you</p>
+      <h3 className="nameprompt">What is your name?</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          className="inputbx"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <button type="submit" className="btn">
+          Submit
+        </button>
+        <button onClick={handleLogout} className="btn">
+          Logout
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default WelcomePage;
