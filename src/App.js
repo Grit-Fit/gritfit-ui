893e143375c64d17ca13cfbbadb8449f3685c1a4
Samay Jain <samayj10@gmail.com>
2025-02-28 19:57:07 +0530

    email:4

--- src/App.js
+++ src/App.js
@@ -1,65 +1,35 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 import Auth from "./components/Auth";
-import LogoPage from "./components/Logo";
 import WelcomePage from "./components/Welcome";
 import GritPhases from "./components/GritPhases";
-import LeftSwipe from "./components/leftSwipe";
-import RightSwipe from "./components/rightSwipe";
-import GFitReport from "./components/gFitReport";
-import CalorieCalculator from "./components/CalorieCalculator";
-import CalorieDisplay from "./components/CalorieDisplay";
-import CalorieTarget from "./components/CalorieTarget";
-import CalorieMacro from "./components/CalorieMacro";
-import NextStepsCarousel from "./components/NextStepsCarousel";
-import NutritionTheory from "./components/NutritionTheory";
-import GymGoal from "./components/GymGoal";
 
-const AppRoutes = () => {
+const App = () => {
     const { accessToken } = useAuth();
 
-    if (!accessToken) {
-        return (
-            <Routes>
-                <Route path="/" element={<Auth />} />
-                <Route path="/welcome" element={<WelcomePage />} />
-                {/* Add other public routes like forgot password if needed */}
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        );
-    }
-
     return (
-        <Routes>
-            <Route path="/" element={<Navigate to="/gritPhases" />} />
-            <Route path="/logo" element={<LogoPage />} />
-            <Route path="/welcome" element={<WelcomePage />} />
-            <Route path="/gritPhases" element={<GritPhases />} />
-            <Route path="/selectTheory" element={<NutritionTheory />} />
-            <Route path="/selectGoal" element={<GymGoal />} />
-            <Route path="/calorieCalc" element={<CalorieCalculator />} />
-            <Route path="/displayCalculation" element={<CalorieDisplay />} />
-            <Route path="/displayTargetCalories" element={<CalorieTarget />} />
-            <Route path="/macros" element={<CalorieMacro />} />
-            <Route path="/nextSteps" element={<NextStepsCarousel />} />
-            <Route path="/leftSwipe" element={<LeftSwipe />} />
-            <Route path="/rightSwipe" element={<RightSwipe />} />
-            <Route path="/gFitReport" element={<GFitReport />} />
-            <Route path="*" element={<Navigate to="/gritPhases" />} />
-        </Routes>
+        <Router>
+            <Routes>
+                {!accessToken ? (
+                    <>
+                        <Route path="/" element={<Auth />} />
+                        <Route path="/signup" element={<Auth mode="signup" />} />
+                        <Route path="/login" element={<Auth mode="login" />} />
+                        <Route path="/welcome" element={<WelcomePage />} />
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="/" element={<Navigate to="/gritPhases" />} />
+                        <Route path="/gritPhases" element={<GritPhases />} />
+                        <Route path="/welcome" element={<WelcomePage />} />
+                        <Route path="*" element={<Navigate to="/gritPhases" />} />
+                    </>
+                )}
+            </Routes>
+        </Router>
     );
 };
-
-const App = () => (
-    <Router>
-        <AppRoutes />
-    </Router>
-);
 
 export default App;
